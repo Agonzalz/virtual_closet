66af51b51824988349507bc362f5a969efbb67ec
agonzalz <asgpersona@gmail.com>
2025-03-17 16:36:04 -0700

    added clothing class

---
+++ closet/clothing.js
@@ -0,0 +1,15 @@
+
+
+ export class Clothing {
+    constructor(name, type, color, image) {
+        this.name = name;
+        this.type = type;
+        this.color = color;
+        this.image = image;
+    }
+
+    displayClothing () {
+        console.log(this)
+    }
+ }
+
